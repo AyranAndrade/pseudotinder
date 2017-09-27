23c80fa97a2ca804a1875083fd96eeaaa63356f7
ayranandrade <ayranandrade@hotmail.com>
2017-09-27 01:01:35 -0300

    Add script to create the database

---
+++ generate-database.sql
@@ -0,0 +1,11 @@
+create database agenda;
+
+use agenda;
+
+create table contato(
+id int not null auto_increment,
+name varchar(60) not null,
+email varchar(255) not null,
+telephone varchar(14) not null,
+primary key (id)
+);
