05ce92fa2f20dd1a53fcc797daf918997127733c
Ayran Andrade <ayranandrade@hotmail.com>
2017-09-30 15:30:18 -0300

    Add a task in the Gulp to copy css source codes to a build folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp');
 
 gulp.task('bootstrap', () => 
     gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
-    .pipe(gulp.dest('src/main/resources/static/css/'))
+    .pipe(gulp.dest('src/main/resources/build/css/'))
 );
 
-gulp.task('default', [ 'bootstrap' ]);
+gulp.task('css', () => 
+    gulp.src('src/main/resources/static/css/*.css')
+    .pipe(gulp.dest('src/main/resources/build/css/'))
+);
+
+gulp.task('default', [ 'bootstrap', 'css' ]);
