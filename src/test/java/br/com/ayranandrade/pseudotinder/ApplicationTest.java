adae851568622a7af9a2d1ff9b8eff0f5f28a9cd
Ayran Andrade <ayranandrade@hotmail.com>
2019-02-22 19:16:23 -0300

    Add test to know if application started correctly

--- src/test/java/br/com/ayranandrade/pseudotinder/ApplicationTest.java
+++ src/test/java/br/com/ayranandrade/pseudotinder/ApplicationTest.java
@@ -1,16 +1,25 @@
 package br.com.ayranandrade.pseudotinder;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.ayranandrade.pseudotinder.controllers.HomeController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class ApplicationTest {
+	
+	@Autowired
+    private HomeController homeController;
 
 	@Test
 	public void contextLoads() {
+		assertNotNull(homeController);
 	}
 
 }
