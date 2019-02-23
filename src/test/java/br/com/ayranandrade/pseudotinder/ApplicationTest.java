5bf3da51ddda8fcbd48a27d131080725d258d953
Ayran Andrade <ayranandrade@hotmail.com>
2019-02-23 16:30:57 -0300

    Add test to know if application responds http requests

--- src/test/java/br/com/ayranandrade/pseudotinder/ApplicationTest.java
+++ src/test/java/br/com/ayranandrade/pseudotinder/ApplicationTest.java
@@ -1,11 +1,14 @@
 package br.com.ayranandrade.pseudotinder;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.ayranandrade.pseudotinder.controllers.HomeController;
@@ -14,6 +17,12 @@ import br.com.ayranandrade.pseudotinder.controllers.HomeController;
 @SpringBootTest
 public class ApplicationTest {
 	
+	@LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
 	@Autowired
     private HomeController homeController;
 
@@ -21,5 +30,12 @@ public class ApplicationTest {
 	public void contextLoads() {
 		assertNotNull(homeController);
 	}
+	
+	@Test
+    public void appMustBeRunningAndRespondingWithLoginPage() throws Exception {
+		String pageReturned = this.restTemplate.getForObject("http://localhost:" + port + "/", 
+				String.class);
+		assertTrue(pageReturned.contains("Bem-vindo ao Pseudotinder!"));
+    }
 
 }
