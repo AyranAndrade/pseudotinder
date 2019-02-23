cef46ea3ac1c021ba269c634d0f335bed71df54c
Ayran Andrade <ayranandrade@hotmail.com>
2019-02-23 16:27:13 -0300

    Refactor Gender class to exclude unnecessary code

--- src/main/java/br/com/ayranandrade/pseudotinder/models/Gender.java
+++ src/main/java/br/com/ayranandrade/pseudotinder/models/Gender.java
@@ -1,8 +1,7 @@
 package br.com.ayranandrade.pseudotinder.models;
 
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Optional;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -22,66 +21,20 @@ public class Gender {
   @Column(name = "gender_id")
   private Integer id;
   
-  @Column(nullable = false)
+  @Column(name = "name", nullable = false)
   @NotNull
   @Enumerated(EnumType.STRING)
-  private GenderType name;
+  private GenderType genderName;
   
-  @Column(name = "created_at", nullable = false)
+  @Column(nullable = false)
   @NotNull
-  private Instant createdAt = Instant.now();
+  private Instant createdAt;
 
-  /**
-   * This constructor is only for JPA use.
-   */
   private Gender() {}
-
-  public Gender(GenderType name) {
-    this.name = Optional.ofNullable(name)
-        .orElseThrow(() -> new IllegalArgumentException("Name of gender can not be null."));
+  
+  @Override
+  public String toString() {
+	  return "Gender [id=" + id + ", genderName=" + genderName + ", createdAt=" + createdAt + "]";
   }
 
-  /**
-   * Given a name not null, this constructor looks for a GenderType 
-   * that value be equal to name ignoring case. Otherwise, a IllegalArgumentException is thrown.
-   */
-  public Gender(String name) {
-    String nameNotNull = Optional.ofNullable(name)
-        .orElseThrow(() -> new IllegalArgumentException("Name of gender can not be null."));
-
-    this.name = Arrays.asList(GenderType.values()).stream()
-        .filter(g -> g.getGenderName().equalsIgnoreCase(nameNotNull))
-        .findFirst()
-        .orElseThrow(() -> new IllegalArgumentException("Name of gender needs " 
-        + "to be a valid value contained in GenderType enum."));
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name.getGenderName();
-  }
-
-  public Instant getCreatedAt() {
-    return createdAt;
-  }
-
-  /**
-   * Set the moment of creation of gender. This value can not be null and needs to be in past.
-   */
-  public void setCreatedAt(Instant createdAt) {
-    Instant createdAtNotNull = Optional.ofNullable(createdAt)
-        .orElseThrow(() -> new IllegalArgumentException("The moment of creation " 
-        + "from a gender can not be null."));
-
-    if (createdAtNotNull.isBefore(Instant.now())) {
-      this.createdAt = createdAtNotNull;
-    } else {
-      throw new IllegalArgumentException("The moment of creation from a gender needs " 
-      + "to be in past.");
-    }
-    
-  }
 }
