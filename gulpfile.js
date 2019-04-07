13ad05bda3c3bb06d008251efed562fa8180ad95
Ayran Andrade <ayranandrade@hotmail.com>
2019-04-06 22:47:36 -0300

    Add the task 'watch' to the Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 const minifyCSS = require('gulp-csso');
 
 function bootstrap() {
@@ -12,6 +12,11 @@ function css() {
     .pipe(dest('src/main/resources/build/css/'))
 }
 
+function watchFiles() {
+  return watch('src/main/resources/css/*.css', css);
+}
+
 exports.css = css;
 exports.bootstrap = bootstrap;
+exports.watch = watchFiles;
 exports.default = parallel(bootstrap, css);
